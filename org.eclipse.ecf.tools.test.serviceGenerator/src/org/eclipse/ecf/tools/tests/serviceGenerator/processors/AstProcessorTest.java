481459fd2b4a4243d46edd546446b94968c8f7c5
salinda <salinda.uwu@gmail.com>
2013-08-07 21:56:36 +0530

    creating the test bundle

---
+++ org.eclipse.ecf.tools.test.serviceGenerator/src/org/eclipse/ecf/tools/tests/serviceGenerator/processors/AstProcessorTest.java
@@ -0,0 +1,54 @@
+package org.eclipse.ecf.tools.tests.serviceGenerator.processors;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AstProcessorTest {
+
+	@Test
+	public final void testAstProcessor() {
+		// TODO
+	}
+
+	@Test
+	public final void testGetNewunit() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetImpleunit() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetImpleunit() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetNewunit() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAddImports() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCretaePackage() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCreateType() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAddMethods() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
