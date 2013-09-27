6660b129adddb0d133b730a39c33cb882ad379b8
Markus Alexander Kuppe <github.com@lemmster.de>
2013-09-27 20:08:40 +0200

    Eclipse e4 compatible BundleActivator (do not need the AbstractUIPlugin anyway)

--- org.eclipse.ecf.tools.serviceGenerator/src/org/eclipse/ecf/tools/serviceGenerator/Activator.java
+++ org.eclipse.ecf.tools.serviceGenerator/src/org/eclipse/ecf/tools/serviceGenerator/Activator.java
@@ -1,28 +1,25 @@
 /*******************************************************************************
-* Copyright (c) 2013 Salinda Jayawardana. All rights reserved. This
-* program and the accompanying materials are made available under the terms of
-* the Eclipse Public License v1.0 which accompanies this distribution, and is
-* available at http://www.eclipse.org/legal/epl-v10.html
-*
-******************************************************************************/
+ * Copyright (c) 2013 Salinda Jayawardana. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ ******************************************************************************/
 package org.eclipse.ecf.tools.serviceGenerator;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+public class Activator implements BundleActivator {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.eclipse.ecf.tools.serviceGenerator"; //$NON-NLS-1$
 
-	// The shared instance
-	private static Activator plugin;
-	
 	public static BundleContext context;
-	
+
 	/**
 	 * The constructor
 	 */
@@ -31,30 +28,25 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
+	@Override
 	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-		Activator.context =context;
+		Activator.context = context;
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
+	@Override
 	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
+		Activator.context = null;
 	}
-
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
-
 }
